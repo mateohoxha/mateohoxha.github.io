5fa6b33b21efdd389589a62eff7158c8f9bba842
Mateo Hoxha <mateohoxha26@gmail.com>
2020-09-02 15:56:17 +0200

    changes in vf

--- vertical-forest/js/main.js
+++ vertical-forest/js/main.js
@@ -9,42 +9,6 @@ function openNav() {
   }
   
   
-  // Slider active class
-  // Cache selectors
-  var lastId,
-    topMenu = $(".indicators"),
-    topMenuHeight = topMenu.outerHeight() + 15,
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function () {
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
-  
-  // Bind to scroll
-  $(window).scroll(function () {
-    // Get container scroll position
-    var fromTop = $(this).scrollTop() + topMenuHeight;
-  
-    // Get id of current scroll item
-    var cur = scrollItems.map(function () {
-      if ($(this).offset().top < fromTop)
-        return this;
-    });
-    // Get the id of the current element
-    cur = cur[cur.length - 1];
-    var id = cur && cur.length ? cur[0].id : "";
-  
-    if (lastId !== id) {
-      lastId = id;
-      // Set/remove active class
-      menuItems
-        .find("div").removeClass("active")
-        .end().filter("[href='#" + id + "']").find("div").addClass("active");
-    }
-  });
-  
   
   // Google Map
   function initMap() {
@@ -67,54 +31,29 @@ function openNav() {
   
   
   
-  
-  $(document).ready(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
-        $('#scroll').fadeIn();
-      } else {
-        $('#scroll').fadeOut();
-      }
-    });
-    $('#scroll').click(function () {
-      $("html, body").animate({ scrollTop: 0 }, 0);
-      return false;
-    });
-  });
-  
-  
-  var body = document.getElementById('body');
   var overlay = document.getElementById('overlay');
   var whiteLogo = document.getElementById('white-logo');
   var heading = document.querySelector('.heading');
   var heroOverlay = document.getElementById('hero-overlay');
-  var indicators = document.querySelector('.indicators');
   var arrow = document.querySelector('.arrowd');
   var links = document.querySelector('.buthead-links');
   
   var tl = new TimelineMax();
   
-  
   var mq = window.matchMedia( "(max-width: 768px)" );
   if (mq.matches) {
     tl.fromTo(whiteLogo, 1, { marginTop: '100px', opacity: '0' }, { marginTop: '0', opacity: '1', ease: Power2.easeInOut })
     .fromTo(overlay, 1, { opacity: '1', zIndex: '100' }, { opacity: '0', zIndex: '-1', ease: Power2.easeInOut })
-    .fromTo(body, 1, { overflow: 'hidden' }, { overflow: 'hidden' }, "-=1")
     .fromTo(heading, 1, { marginTop: '50px', opacity: '0' } , { marginTop: '0', opacity: '1', ease: Power2.easeInOut })
     .fromTo(heroOverlay, 1, { width: '0' }, { width: '50%', eaes: Power2.easeInOut })
-    .fromTo(indicators, 1, { opacity: '0', right: '-84px' }, { opacity: '1', right: '20px', ease: Power2.easeInOut })
     .fromTo(arrow, 1, { opacity: '0', bottom: '0' }, { opacity: '1', bottom: '80', ease: Power2.easeInOut }, "-=1")
-    // .fromTo(links, 1, { opacity: '0', padding: '60px 0' }, { opacity: '1', padding: '50px 0 100px 0', ease: Power2.easeInOut }, "-=1")
   }
   else {
     tl.fromTo(whiteLogo, 1, { marginTop: '100px', opacity: '0' }, { marginTop: '0', opacity: '1', ease: Power2.easeInOut })
     .fromTo(overlay, 1, { opacity: '1', zIndex: '100' }, { opacity: '0', zIndex: '-1', ease: Power2.easeInOut })
-    .fromTo(body, 1, { overflow: 'hidden' }, { overflow: 'hidden' }, "-=1")
     .fromTo(heading, 1, { marginTop: '50px', opacity: '0' } , { marginTop: '0', opacity: '1', ease: Power2.easeInOut })
     .fromTo(heroOverlay, 1, { width: '0' }, { width: '50%', eaes: Power2.easeInOut })
-    .fromTo(indicators, 1, { opacity: '0', right: '-84px' }, { opacity: '1', right: '84px', ease: Power2.easeInOut })
     .fromTo(arrow, 1, { opacity: '0', bottom: '0' }, { opacity: '1', bottom: '80', ease: Power2.easeInOut }, "-=1")
-    // .fromTo(links, 1, { opacity: '0', padding: '60px 0' }, { opacity: '1', padding: '60px 120px', ease: Power2.easeInOut }, "-=1")
   }
 
 
